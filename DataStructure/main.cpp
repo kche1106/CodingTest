1d7177af98f5d1eb25b8df923065dc8cc07eeccc
kche1106 <kche011106@gmail.com>
2022-07-13 20:20:59 +0900

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by 김채은 on 2022/07/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
