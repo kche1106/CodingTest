b4dce85a91e9b54f9db18798cf9e30e70c14215d
kche1106 <kche011106@gmail.com>
2022-07-14 20:05:42 +0900

    10828 해결

--- DataStructure/10828.cpp
+++ DataStructure/10828.cpp
@@ -5,4 +5,59 @@
 //  Created by 김채은 on 2022/07/13.
 //
 
-#include "10828.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void stack(string);
+int arr[100];
+int i = 0;
+
+int main() {
+    
+    int N;
+    cin >> N;
+    
+    string s;
+    for(int i = 0; i < N; i++) {
+        cin >> s;
+        stack(s);
+    }
+}
+
+void stack(string s) {
+    
+    if(s == "push") {
+        int n;
+        cin >> n;
+        arr[i++] = n;
+    }
+    
+    else if(s == "pop") {
+        if (i == 0)
+            cout << "-1" << endl;
+        else {
+            cout << arr[i - 1] << endl;
+            arr[i--] = 0;
+        }
+    }
+    
+    else if(s == "size") {
+        cout << i << endl;
+    }
+    
+    else if(s == "empty") {
+        if (i == 0)
+            cout << "1" << endl;
+        else
+            cout << "0" << endl;
+    }
+    
+    else if(s == "top") {
+        if (i == 0)
+            cout << "-1" << endl;
+        else {
+            cout << arr[i - 1] << endl;
+        }
+    }
+}
