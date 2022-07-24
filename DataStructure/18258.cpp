a7e08f8444375be8de067880e54b342d215d918a
kche1106 <kche011106@gmail.com>
2022-07-24 21:38:31 +0900

    18258 런타임 에러

--- DataStructure/18258.cpp
+++ DataStructure/18258.cpp
@@ -9,14 +9,63 @@
 #include <string>
 using namespace std;
 
-int arr[100];
-int i = 0;
-int j = 0;
+class Queue {
+private:
+    int arr[100];
+    int i = 0; //다음에 들어갈 곳
+    int j = 0; //시작
+    int size = 0;
+    
+public:
+    void push(int n) {
+        size++;
+        arr[i++] = n;
+    }
+    
+    void pop() {
+        if (size == 0)
+            cout << "-1" << endl;
+        else {
+            cout << arr[j] << endl;
+            arr[j++] = 0;
+            size--;
+        }
+    }
+    
+    void sizeF() {
+        cout << size << endl;
+    }
+    
+    void empty() {
+        if (size == 0)
+            cout << "1" << endl;
+        else
+            cout << "0" << endl;
+    }
+    
+    void front() {
+        if (size == 0)
+            cout << -1 << endl;
+        else {
+            cout << arr[j] << endl;
+        }
+    }
+    
+    void back() {
+        if (size == 0)
+            cout << -1 << endl;
+        else {
+            cout << arr[i-1] << endl;
+        }
+    }
+};
 
 int main() {
     
     int N;
     cin >> N;
+    
+    Queue q;
     
     string s;
     for(int i = 0; i < N; i++) {
@@ -24,43 +73,29 @@ int main() {
         if(s == "push") {
             int n;
             cin >> n;
-            arr[i++] = n;
+            q.push(n);
         }
         
         else if(s == "pop") {
-            if (i == 0 || j >= i)
-                cout << "-1" << endl;
-            else {
-                cout << arr[j] << endl;
-                arr[j++] = 0;
-            }
+            q.pop();
         }
         
         else if(s == "size") {
-            cout << i-j << endl;
+            q.sizeF();
         }
         
         else if(s == "empty") {
-            if (i == 0 || j >= i)
-                cout << "1" << endl;
-            else
-                cout << "0" << endl;
+            q.empty();
         }
         
         else if(s == "front") {
-            if (i == 0)
-                cout << -1 << endl;
-            else {
-                cout << arr[j] << endl;
-            }
+            q.front();
         }
         
         else if(s == "back") {
-            if (i == 0)
-                cout << -1 << endl;
-            else {
-                cout << arr[i-1] << endl;
-            }
+            q.back();
         }
     }
 }
+
+
