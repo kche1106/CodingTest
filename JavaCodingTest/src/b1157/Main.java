6e2f25f731f8ad9c2a54e35d06e183cbcbf64c38
kche1106 <kche011106@gmail.com>
2023-10-20 12:14:47 +0900

    1157 해결

---
+++ JavaCodingTest/src/b1157/Main.java
@@ -0,0 +1,29 @@
+package b1157;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int arr[] = new int[26];
+
+        for(int i = 0; i < str.length(); i++) {
+            if((str.charAt(i)) >= 'A' && str.charAt(i) <= 'Z') arr[str.charAt(i) - 'A']++;
+            else arr[str.charAt(i) - 'a']++;
+        }
+
+        int max = 0;
+        char ch = '?';
+        for(int i = 0; i < arr.length; i++) {
+            if(max < arr[i]) {
+                max = arr[i];
+                ch = (char) (i + 'A');
+            }
+            else if(arr[i] == max)
+                ch = '?';
+        }
+
+        System.out.println(ch);
+    }
+}
