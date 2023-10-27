54f53aefd060a3939085f9bd24d323c84a3dccdd
kche1106 <kche011106@gmail.com>
2023-10-27 15:04:52 +0900

    1204 해결 (re)

---
+++ SWEA/SWEA/1204.cpp
@@ -0,0 +1,41 @@
+//
+//  1204.cpp
+//  SWEA
+//
+//  Created by 김채은 on 2023/10/27.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        
+        int scores[101] = {0, };
+        for(int j = 0; j < 1000; j++) {
+            int score;
+            cin >> score;
+            scores[score]++;
+        }
+        
+        cout << "입력 완료" << endl;
+        
+        int max = 0, idx = 0;
+        
+        for(int k = 0; k < 100; k++) {
+            if(max <= scores[k]) {
+                max = scores[k];
+                idx = k;
+            }
+        }
+        
+        cout << "#" << i << " " << idx << endl;
+    }
+    
+    return 0;
+}
