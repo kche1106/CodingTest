3df7fce0bc3a9c41140b1830ccddfa52a497f764
kche1106 <kche011106@gmail.com>
2023-12-19 23:29:26 +0900

    11722 고민

--- DataStructure/11722.cpp
+++ DataStructure/11722.cpp
@@ -30,3 +30,14 @@ int main() {
     
     cout << *max_element(dp, dp+n) << '\n';
 }
+
+/*
+ dp[n] = 1;
+ for(int i = n-1; i > 0; i--) {
+     for(int j = i+1; j <= n; j++) {
+         if(arr[i] > arr[j]) {
+            dp[i] = max(dp[j] + 1, dp[i]);
+         }
+     }
+ }
+ */
