701024ccf23e64307786e9aa94134eb4a540020a
kche1106 <kche011106@gmail.com>
2024-01-07 15:03:31 +0900

    [BFS] 2178 해결

--- DataStructure/2178.cpp
+++ DataStructure/2178.cpp
@@ -12,31 +12,9 @@ using namespace std;
 int arr[101][101], dist[101][101];
 queue<pair<int, int>> q;
 int n, m;
-//int res = 10001;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-//void DFS(int x, int y, int cnt) {
-//        
-//    if(x == n && y == m) {
-//        if(res > cnt) res = cnt;
-//        return;
-//    }
-//    
-//    if(x <= 0 || y <= 0 || x > n || y > m) return;
-//        
-//    for(int i = 0; i < 4; i++) {
-//        int nx = x + dx[i];
-//        int ny = y + dy[i];
-//        
-//        if(arr[nx][ny] == 1 && visited[nx][ny] != 1) {
-//            visited[nx][ny] = 1;
-//            DFS(nx, ny, cnt+1);
-//            visited[nx][ny] = 0;
-//        }
-//    }
-//    
-//}
 
 void BFS() {
     
@@ -81,9 +59,7 @@ int main() {
             arr[i][j] = temp[j] - '0';
         }
     }
-    
-//    DFS(1, 1, 1);
-    
+        
     q.push(make_pair(0,0));
     dist[0][0] = 1;
     BFS();
