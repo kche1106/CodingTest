f8a97628eb6fc19e22b07b1738a5170fe03a7e6f
kche1106 <kche011106@gmail.com>
2024-01-19 12:21:16 +0900

    2583 해결

--- DataStructure/2583.cpp
+++ DataStructure/2583.cpp
@@ -6,21 +6,22 @@
 //
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int m, n, k;
 int map[100][100];
 queue<pair<int, int>> q;
-int dx[] = {-1, 1, 0, 0};
+int dx[] = {-1, 1 ,0, 0};
 int dy[] = {0, 0, -1, 1};
-vector<int> ans;
 int sum;
+vector<int> ans;
 
 void BFS() {
     while(!q.empty()) {
+        sum++;
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
@@ -30,11 +31,9 @@ void BFS() {
             int ny = y + dy[i];
             
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            
             if(map[nx][ny] != 1) {
-                sum += 1;
                 map[nx][ny] = 1;
-                q.push(make_pair(nx, ny));
+                q.push({nx, ny});
             }
         }
     }
@@ -47,9 +46,9 @@ int main() {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
         
-        for(int i = x1; i < x2; i++) {
-            for(int j = y1; j < y2; j++) {
-                map[i][j] = 1;
+        for(int j = x1; j < x2; j++) {
+            for(int k = y1; k < y2; k++) {
+                map[j][k] = 1;
             }
         }
     }
@@ -58,10 +57,10 @@ int main() {
         for(int j = 0; j < m; j++) {
             if(map[i][j] != 1) {
                 map[i][j] = 1;
-                q.push(make_pair(i, j));
-                sum = 1;
+                q.push({i, j});
                 BFS();
                 ans.push_back(sum);
+                sum = 0;
             }
         }
     }
