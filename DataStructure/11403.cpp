0889a06bf4a7618db2d7c3bae068e5ef021ce1f2
kche1106 <kche011106@gmail.com>
2024-01-22 10:58:57 +0900

    11403 해결

--- DataStructure/11403.cpp
+++ DataStructure/11403.cpp
@@ -6,35 +6,30 @@
 //
 
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int res[100][100];
 
 int main() {
     int n;
     cin >> n;
     
-    int graph[100][100];
+    int map[100][100];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            cin >> graph[i][j];
-            
-            if(graph[i][j] == 1) res[i][j] = 1;
+            cin >> map[i][j];
         }
     }
     
-    for(int i = 0; i < n; i++) {  //거쳐가는 정점
-        for(int j = 0; j < n; j++) {  //출발
-            for(int k = 0; k < n; k++) {  //도착
-                    if(res[j][i] && res[i][k] == 1) res[j][k] = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                if(map[j][i] == 1 && map[i][k] == 1) map[j][k] = 1;
             }
         }
     }
     
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            cout << res[i][j] << " ";
+            cout <<  map[i][j] << " ";
         }
         cout << endl;
     }
