c8c9b791bf50321f0e729f9228777a7009ef0868
kche1106 <kche011106@gmail.com>
2024-01-23 19:09:37 +0900

    7569 해결 (re)

--- DataStructure/7569.cpp
+++ DataStructure/7569.cpp
@@ -8,15 +8,14 @@
 #include <iostream>
 #include <queue>
 #include <tuple>
-#include <algorithm>
 using namespace std;
 
 int m, n, h;
 int box[100][100][100];
 int dist[100][100][100];
-int dm[] = {-1, 1, 0, 0, 0, 0};
+int dh[] = {-1, 1, 0, 0, 0, 0};
 int dn[] = {0, 0, -1, 1, 0, 0};
-int dh[] = {0, 0, 0, 0, -1, 1};
+int dm[] = {0, 0, 0, 0, -1, 1};
 queue<tuple<int, int, int>> q;
 
 void BFS() {
@@ -30,44 +29,61 @@ void BFS() {
             int nh = qh + dh[i];
             int nn = qn + dn[i];
             int nm = qm + dm[i];
-                        
-            if(nh < 0 || nh >= h || nn < 0 || nn >= n || nm < 0 || nm >= m) continue;
-            if(dist[nh][nn][nm] == -1) {  //안익은 토마토
+            
+            if(nh < 0 || nm < 0 || nn < 0 || nh >= h || nm >= m || nn >= n) continue;
+            
+            if(box[nh][nn][nm] == 0) {
+                box[nh][nn][nm] = 1;
                 dist[nh][nn][nm] = dist[qh][qn][qm] + 1;
-                q.push(make_tuple(nh, nn, nm));
+                q.push({nh, nn, nm});
             }
+            
         }
     }
 }
 
 int main() {
-    cin >> m >> n >> h;
+    cin >> m >> n >> h;  //가로, 세로, 높이
     
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < n; j++) {
             for(int k = 0; k < m; k++) {
-                cin >> box[i][j][k];
-                if(box[i][j][k] == 1) q.push(make_tuple(i, j, k));  //익은 토마토 큐에 넣어줌
-                else if(box[i][j][k] == 0) dist[i][j][k] = -1;  //안익은 토마토
+                cin >> box[i][j][k];  //높이, 세로, 가로
+                if(box[i][j][k] == 0) dist[i][j][k] = -1;
+            }
+        }
+    }
+    
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < m; k++) {
+                if(box[i][j][k] == 1) q.push({i, j, k});  //안익은 토마토
             }
         }
     }
     
     BFS();
     
-    int day = 0;
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < n; j++) {
             for(int k = 0; k < m; k++) {
-                //익지 않은 토마토가 있으면
-                if(dist[i][j][k] == -1) {
+                if(dist[i][j][k] == -1) {  //안익은 토마토가 남아있음
                     cout << "-1" << endl;
                     return 0;
                 }
-                day = max(day, dist[i][j][k]);  //처음부터 토마토가 다 익어있으면 dist 배열에는 0밖에 없음
             }
         }
     }
     
-    cout << day << endl;
+    int res = 0;
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < m; k++) {
+                if(res < dist[i][j][k]) res = dist[i][j][k];
+            }
+        }
+    }
+    
+    cout << res << endl;
+    
 }
