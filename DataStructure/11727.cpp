4eead49c25791a6a07da155b6ef1085f7d9eebc6
kche1106 <kche011106@gmail.com>
2024-02-06 18:28:33 +0900

    [DP] 11727 해결 (re)

---
+++ DataStructure/11727.cpp
@@ -0,0 +1,23 @@
+//
+//  11727.cpp
+//  DataStructure
+//
+//  Created by 김채은 on 2024/02/06.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int dp[1000];
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+    }
+    
+    cout << dp[n];
+}
