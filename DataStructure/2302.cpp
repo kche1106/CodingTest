322313cf9dbcf2ad2379b53ebcd37267181dbe86
kche1106 <kche011106@gmail.com>
2024-02-17 23:47:29 +0900

    2302 해결

--- DataStructure/2302.cpp
+++ DataStructure/2302.cpp
@@ -19,7 +19,7 @@ int main() {
         cin >> v;
         vip.push_back(v);
     }
-        
+    
     int dp[41];
     dp[0] = 1;
     dp[1] = 1;
@@ -27,23 +27,16 @@ int main() {
     for(int i = 3; i <= n; i++) {
         dp[i] = dp[i-1] + dp[i-2];
     }
-        
-    int start;
-    int end;
     
+    int start = 1;
+    int end = 0;
     int res = 1;
-    for(int i = 0; i <= m; i++) {
-        if(i == 0) start = 1;
-        else start = vip[i-1] + 1;
+    for(int i = 0; i < vip.size()+1; i++) {
+        if(i == vip.size()) end = n+1;
+        else end = vip[i];
         
-        if(i == m) end = n;
-        else end = vip[i] - 1;
-                
-        int cnt = 0;
-        for(int j = start; j <= end; j++) cnt++;
-        
-        res = res * dp[cnt];
+        res *= dp[end-start];
+        start = end + 1;
     }
-    
     cout << res;
 }
