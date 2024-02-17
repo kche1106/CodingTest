89b21f0ccfbe75aacfdb3f51807dfb113e282b36
kche1106 <kche011106@gmail.com>
2024-02-17 13:14:29 +0900

    12852 해결

--- DataStructure/12852.cpp
+++ DataStructure/12852.cpp
@@ -6,18 +6,12 @@
 //
 
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int dp[1000001];
 int path[1000001];
 
 int main() {
-    
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
     int n;
     cin >> n;
     
@@ -26,19 +20,23 @@ int main() {
         path[i] = i-1;
         
         if(i % 3 == 0) {
-            dp[i] = min(dp[i/3] + 1, dp[i]);
-            path[i] = (dp[i/3] + 1 > dp[i]) ? path[i] : i/3;
+            if(dp[i] > dp[i/3] + 1) {
+                dp[i] = dp[i/3] + 1;
+                path[i] = i/3;
+            }
         }
         if(i % 2 == 0) {
-            dp[i] = min(dp[i/2] + 1, dp[i]);
-            path[i] = (dp[i/2] + 1 > dp[i]) ? path[i] : i/2;
+            if(dp[i] > dp[i/2] + 1) {
+                dp[i] = dp[i/2] + 1;
+                path[i] = i/2;
+            }
         }
     }
     
     cout << dp[n] << endl;
-    cout << n << " ";
-    while (n > 1) {
-        n = path[n];
+    
+    while(n > 0) {
         cout << n << " ";
+        n = path[n];
     }
 }
