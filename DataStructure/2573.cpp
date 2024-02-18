a73068790bac659d4957aa2f06f24692076f8608
kche1106 <kche011106@gmail.com>
2024-02-18 18:15:28 +0900

    [DFS] 2573 해결

--- DataStructure/2573.cpp
+++ DataStructure/2573.cpp
@@ -11,48 +11,32 @@
 using namespace std;
 
 int n, m;
-int map[300][300];
-int visited[300][300];
-int temp[300][300];
-queue<pair<int, int>> q;
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, 1, -1};
+int map[301][301];
+int visited[301][301];
 
-void check(int x, int y) {
-    q.push({x, y});
-    visited[x][y] = 1;
-    
-    while (!q.empty()) {
-        int qx = q.front().first;
-        int qy = q.front().second;
-        q.pop();
-        
-        for(int i = 0; i < 4; i++) {
-            int nx = qx + dx[i];
-            int ny = qy + dy[i];
-            
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if(visited[nx][ny] == 0 && map[nx][ny] > 0) {
-                visited[nx][ny] = 1;
-                q.push({nx, ny});
-            }
-        }
-    }
-}
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
 void melt() {
+    int temp[301][301];
     memset(temp, 0, sizeof(temp));
     
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
+            if(map[i][j] == 0) continue;
+            
             int cnt = 0;
-            for(int k = 0; k < 4; k++) {
-                int nx = i + dx[k];
-                int ny = j + dy[k];
+            for(int d = 0; d < 4; d++) {
+                int nx = i + dx[d];
+                int ny = j + dy[d];
+                
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                
                 if(map[nx][ny] == 0) cnt++;
             }
-            if(map[i][j] - cnt > 0) temp[i][j] = map[i][j] - cnt;
+            
+            temp[i][j] = map[i][j] - cnt;
+            if(temp[i][j] < 0) temp[i][j] = 0;
         }
     }
     
@@ -63,39 +47,55 @@ void melt() {
     }
 }
 
+void check(int x, int y) {
+    visited[x][y] = 1;
+    
+    for(int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+        
+        if(!visited[nx][ny] && map[nx][ny] > 0) {
+            visited[nx][ny] = 1;
+            check(nx, ny);
+        }
+    }
+}
+
 int main() {
     cin >> n >> m;
-    
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> map[i][j];
         }
     }
     
-    int year = 0;
-    while(true) {
+    int res = 0;
+    while (true) {
+        res += 1;
+        melt();
+        
         int cnt = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(visited[i][j] == 0 && map[i][j] > 0) {
-                    check(i, j);
+                if(map[i][j] > 0 && !visited[i][j]) {
                     cnt++;
+                    visited[i][j] = 1;
+                    check(i, j);
                 }
             }
         }
-        
-        if(cnt == 0) {
-            cout << "0";
-            break;
-        }  //cnt가 1이면 아직 분리되기 전이므로 계속 반복
-        else if(cnt >= 2) {
-            cout << year;
+        memset(visited, 0, sizeof(visited));
+                
+        if(cnt >= 2) {
+            cout << res;
             break;
         }
-        
-        year += 1;
-        melt();
-        
-        memset(visited, 0, sizeof(visited));
+        else if(cnt == 0) {
+            cout << "0";
+            break;
+        }
     }
 }
