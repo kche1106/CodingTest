ad81e5e119dca97ead1715a7ba0c702b28843a16
kche1106 <kche011106@gmail.com>
2024-02-18 17:39:46 +0900

    [DP] 11057 해결

--- DataStructure/11057.cpp
+++ DataStructure/11057.cpp
@@ -12,18 +12,19 @@ int main() {
     int n;
     cin >> n;
     
-    int dp[1001][10];
+    int dp[n+1][11];
     for(int i = 1; i <= n; i++) {
-        for(int j = 0; j <= 9; j++) {
-            if(i == 1 || j == 0) dp[i][j] = 1;
+        for(int j = 0; j < 10; j++) {
+            if(i == 1) dp[i][j] = 1 % 10007;
+            else if(j == 0) dp[i][j] = 1 % 10007;
             else dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 10007;
         }
     }
     
-    int sum = 0;
-    for(int i = 0; i <= 9; i++) {
-        sum += dp[n][i];
+    int res = 0;
+    for(int i = 0; i < 10; i++) {
+        res += dp[n][i] % 10007;
     }
     
-    cout << sum % 10007;
+    cout << res % 10007;
 }
