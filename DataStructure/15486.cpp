57ef8a5f3a8284fe93ede69a09470f018652ee88
kche1106 <kche011106@gmail.com>
2024-02-18 18:36:55 +0900

    [DP] 15486 해결

--- DataStructure/15486.cpp
+++ DataStructure/15486.cpp
@@ -6,35 +6,24 @@
 //
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+
+int dp[1500001];
 
 int main() {
     int n;
     cin >> n;
     
-    vector<int> p;
-    vector<int> t;
-    for(int i = 0; i < n; i++) {
-        int a, b;
-        cin >> a >> b;
-        t.push_back(a);
-        p.push_back(b);
+    int t[1500001];
+    int p[1500001];
+    for(int i = 1; i <= n; i++) {
+        cin >> t[i] >> p[i];
     }
     
-    vector<int> dp; //전역변수로 쓰면 에러남
-    for(int i = 0; i <= n; i++) {
-        dp.push_back(0);
+    for(int i = n; i >= 1; i--) {
+        if(i + t[i] > n+1) dp[i] = dp[i+1];
+        else dp[i] = max(dp[i+1], p[i] + dp[i + t[i]]);
     }
     
-    for(int i = n-1; i >= 0; i--) {
-        int next = i + t[i];
-        if(next > n) dp[i] = dp[i+1];
-        else {
-            dp[i] = max(dp[i+1], dp[next] + p[i]);
-        }
-    }
-    
-    cout << dp[0];
+    cout << dp[1];
 }
