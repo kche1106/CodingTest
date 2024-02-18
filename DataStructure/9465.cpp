4492af49f76e57a83824b32780efb0fd6f1b0689
kche1106 <kche011106@gmail.com>
2024-02-18 17:19:06 +0900

    [DP] 9465 해결

--- DataStructure/9465.cpp
+++ DataStructure/9465.cpp
@@ -17,26 +17,25 @@ int main() {
         int n;
         cin >> n;
         
-        int sticker[2][100001] = {0, };
+        int a[2][100001] = {0, };
+        int dp[2][100001] = {0, };
+        
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < n; j++) {
-                cin >> sticker[i][j];
+                cin >> a[i][j];
             }
         }
         
-        int dp[2][100001] = {0, };
-        dp[0][0] = sticker[0][0];
-        dp[0][1] = sticker[1][0] + sticker[0][1];
-        dp[1][0] = sticker[1][0];
-        dp[1][1] = sticker[0][0] + sticker[1][1];
+        dp[0][0] = a[0][0];
+        dp[1][0] = a[1][0];
+        dp[0][1] = a[0][1] + a[1][0];
+        dp[1][1] = a[1][1] + a[0][0];
         
         for(int j = 2; j < n; j++) {
-            //(0,0)에서 시작
-            dp[0][j] = sticker[0][j] + max(dp[1][j-1], dp[1][j-2]);
-            //(1,0)에서 시작
-            dp[1][j] = sticker[1][j] + max(dp[0][j-1], dp[0][j-2]);
-        }
+            dp[0][j] = a[0][j] + max(dp[1][j-1], dp[1][j-2]);
+            dp[1][j] = a[1][j] + max(dp[0][j-1], dp[0][j-2]);
             
+        }
         cout << max(dp[0][n-1], dp[1][n-1]) << endl;
     }
 }
