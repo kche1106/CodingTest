013dea23c9d7df0c983ebb2cddd7235858420e1a
kche1106 <kche011106@gmail.com>
2024-02-19 20:00:56 +0900

    [큐] 11286 해결 (re)

---
+++ DataStructure/11286.cpp
@@ -0,0 +1,44 @@
+//
+//  11286.cpp
+//  DataStructure
+//
+//  Created by 김채은 on 2024/02/19.
+//
+
+#include <iostream>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+struct comp {
+    bool operator() (int a, int b) {
+        if(abs(a) < abs(b)) return false;
+        else if(abs(a) == abs(b)) {
+            return a > b;
+        }
+        else return true;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    priority_queue<int, vector<int>, comp> q;
+    
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        
+        if(x == 0) {
+            if(q.empty()) cout << "0\n";
+            else {
+                cout << q.top() << '\n';
+                q.pop();
+            }
+        }
+        else {
+            q.push(x);
+        }
+    }
+}
