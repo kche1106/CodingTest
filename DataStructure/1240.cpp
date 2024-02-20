d090068b17ded693665037a136eb8027fc08696e
kche1106 <kche011106@gmail.com>
2024-02-20 23:45:02 +0900

    [BFS] 1240 해결 (re)

--- DataStructure/1240.cpp
+++ DataStructure/1240.cpp
@@ -6,27 +6,39 @@
 //
 
 #include <iostream>
+#include <queue>
 #include <cstring>
 using namespace std;
 
-//bfs로 풀어보기
-
 int n, m;
-int map[1001][1001];
+vector<pair<int, int>> map[1001];
+queue<int> q;
 int visited[1001];
-int cost = 1000000000;
+int res;
 
-void DFS(int x, int y, int res) {
-    if(x == y) {
-        if(cost > res) cost = res;
-        return;
-    }
+void BFS(int start, int dest) {
+    queue<pair<int, int>> q;
+    q.push({start, 0});
+    visited[start] = 1;
+    res = 0;
     
-    for(int i = 1; i <= n; i++) {
-        if(map[x][i] > 0 && visited[i] == 0) {
-            visited[i] = 1;
-            DFS(i, y, res+map[x][i]);
-            visited[i] = 0;
+    while(!q.empty()) {
+        int x = q.front().first;
+        int dist = q.front().second;
+        q.pop();
+        
+        if(x == dest) {
+            res = dist;
+        }
+        
+        for(int i = 0; i < map[x].size(); i++) {            
+            int nx = map[x][i].first;
+            int ndist = map[x][i].second;
+            
+            if(!visited[nx]) {
+                visited[nx] = 1;
+                q.push({nx, ndist + dist});
+            }
         }
     }
 }
@@ -35,22 +47,26 @@ int main() {
     cin >> n >> m;
     
     for(int i = 0; i < n-1; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        map[a][b] = c;
-        map[b][a] = c;
+        int a, b, d;
+        cin >> a >> b >> d;
+        map[a].push_back({b, d});
+        map[b].push_back({a, d});
     }
     
     for(int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         
-        visited[a] = 1;
-        DFS(a, b, 0);
-        
-        cout << cost << endl;
-        
-        cost = 1000000000;
+        BFS(a, b);
+        cout << res << endl;
         memset(visited, 0, sizeof(visited));
     }
 }
+/*
+ 4 2
+ 2 1 2
+ 4 3 2
+ 1 4 3
+ 1 2
+ 3 2
+ */
