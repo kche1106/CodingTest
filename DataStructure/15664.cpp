5adf5b8bbb40bad010523c9d96959e4aef1c52a7
kche1106 <kche011106@gmail.com>
2024-03-03 14:46:55 +0900

    [백트래킹] 15664 해결

--- DataStructure/15664.cpp
+++ DataStructure/15664.cpp
@@ -5,4 +5,37 @@
 //  Created by 김채은 on 2024/03/03.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+int res[9];
+
+void dfs(int l, int cnt) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) cout << res[i] << " ";
+        cout << "\n";
+        return;
+    }
+    
+    int pre = 0;
+    for(int i = l+1; i < n; i++) {
+        if(pre != arr[i]) {
+            res[cnt] = arr[i];
+            pre = res[cnt];
+            dfs(i, cnt+1);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    
+    dfs(-1, 0);
+}
