46d6e61dc55d9095da8c78e4646c9f7d00aae7c9
kche1106 <kche011106@gmail.com>
2024-03-03 14:47:12 +0900

    [백트래킹] 15666 해결

---
+++ DataStructure/15666.cpp
@@ -0,0 +1,40 @@
+//
+//  15666.cpp
+//  DataStructure
+//
+//  Created by 김채은 on 2024/03/03.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+int res[9];
+
+void dfs(int l, int cnt) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) cout << res[i] << " ";
+        cout << "\n";
+        return;
+    }
+    
+    int pre = 0;
+    for(int i = l; i < n; i++) {
+        if(pre != arr[i]) {
+            pre = arr[i];
+            res[cnt] = arr[i];
+            dfs(i, cnt+1);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+    
+    dfs(0, 0);
+}
