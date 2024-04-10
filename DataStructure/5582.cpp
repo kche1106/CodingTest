b0495e87d4cb35452e808c557e76842ad41bca51
kche1106 <kche011106@gmail.com>
2024-04-10 13:28:54 +0900

    [DP] 5582 해결 (re) /

--- DataStructure/5582.cpp
+++ DataStructure/5582.cpp
@@ -6,24 +6,37 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int dp[4001][4001];
-
 int main() {
+
     string s1, s2;
     cin >> s1 >> s2;
-    
-    int res = 0;
+
+    vector<vector<int>> dp(s1.length(), vector<int>(s2.length()));
+
+    int ans = 0;
     for(int i = 0; i < s1.length(); i++) {
         for(int j = 0; j < s2.length(); j++) {
             if(s1[i] == s2[j]) {
-                dp[i][j] = 1;
-                if(i >= 1 && j >= 1) dp[i][j] += dp[i-1][j-1];
+                if(i >= 1 && j >= 1) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else {
+                    dp[i][j] = 1;
+                }
+                ans = max(ans, dp[i][j]);
             }
-            if(res < dp[i][j]) res = dp[i][j];
         }
     }
-    
-    cout << res;
+
+    for(int i = 0; i < s1.length(); i++) {
+        for(int j = 0; j < s2.length(); j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << ans;
 }
