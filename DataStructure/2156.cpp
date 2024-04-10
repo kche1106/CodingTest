f2bbd51f998640f87a6da15649441019fc2953a5
kche1106 <kche011106@gmail.com>
2024-04-10 14:36:21 +0900

    [DP] 2156 해결 /

--- DataStructure/2156.cpp
+++ DataStructure/2156.cpp
@@ -6,27 +6,28 @@
 //
 
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    int a[10001];
+
+    int grape[10001];
     for(int i = 0; i < n; i++) {
-        cin >> a[i];
+        cin >> grape[i];
     }
-    
-    int dp[10001];
-    dp[0] = a[0];
-    dp[1] = dp[0] + a[1];
-    dp[2] = max(a[0] + a[1], a[0] + a[2]);
-    dp[2] = max(dp[2], a[1] + a[2]);
-    
-    for(int i = 3; i <= n; i++) {
-        dp[i] = max(dp[i-1], a[i] + a[i-1] + dp[i-3]);
-        dp[i] = max(dp[i], a[i] + dp[i-2]);
+
+    vector<int> dp(n, 0);
+    dp[0] = grape[0];
+    dp[1] = grape[0] + grape[1];
+    dp[2] = max(grape[0], grape[1]) + grape[2];
+    dp[2] = max(dp[2], dp[1]);
+
+    for(int i = 3; i < n; i++) {
+        dp[i] = max(dp[i-2], grape[i-1] + dp[i-3]) + grape[i]; 
+        dp[i] = max(dp[i], dp[i-1]); 
     }
-    
+
     cout << dp[n-1];
 }
