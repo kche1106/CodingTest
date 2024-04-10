b7e08fac4d78c14eb8e8bec6a80b6e7d985893a2
kche1106 <kche011106@gmail.com>
2024-04-10 17:03:36 +0900

    [DP] 11660 해결 /

--- DataStructure/11660.cpp
+++ DataStructure/11660.cpp
@@ -9,9 +9,6 @@
 #include <vector>
 using namespace std;
 
-int dp[1025][1025];
-int a[1025][1025];
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -20,17 +17,28 @@ int main() {
     int n, m;
     cin >> n >> m;
 
+    vector<vector<int>> map(n+1, vector<int> (n+1));
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            cin >> a[i][j];
-            dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + a[i][j];
+            cin >> map[i][j];
         }
     }
-    
-    int x1, x2, y1, y2;
+
+    vector<vector<int>> dp(n+1, vector<int> (n+1));
+    for(int i = 1; i < n+1; i++) dp[1][i] = dp[1][i-1] + map[1][i];
+    for(int i = 1; i < n+1; i++) dp[i][1] = dp[i-1][1] + map[i][1];
+    for(int i = 2; i < n+1; i++) {
+        for(int j = 2; j < n+1; j++) {
+            dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + map[i][j];        
+        }
+    }
+
+    int x1, y1, x2, y2;
     for(int i = 0; i < m; i++) {
         cin >> x1 >> y1 >> x2 >> y2;
-        
-        cout << dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1] << '\n';
+
+        int ans = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+
+        cout << ans << "\n";
     }
 }
