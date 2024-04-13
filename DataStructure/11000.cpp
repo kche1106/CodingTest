e5d17d0e7f0a17a2948a1e146293293b5fe8efb1
kche1106 <kche011106@gmail.com>
2024-04-13 11:42:55 +0900

    [그리디] 11000 해결 (re) /

--- DataStructure/11000.cpp
+++ DataStructure/11000.cpp
@@ -11,32 +11,36 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main()
+{
    int n;
    cin >> n;
 
    priority_queue<int, vector<int>, greater<int>> pq;
-    vector<pair<int, int>> v;
-   for (int i = 0; i < n; i++) {
+   vector<pair<int, int>> v;
+   for (int i = 0; i < n; i++)
+   {
       int a, b;
       cin >> a >> b;
 
-      v.push_back({a, b});  //end, start
+      v.push_back({a, b}); // end, start
    }
-    sort(v.begin(), v.end());
+   sort(v.begin(), v.end());
 
-    pq.push(v[0].second);
-    
-    for(int i = 1; i < v.size(); i++) {
-       
-        int start = v[i].first;
-        int end = v[i].second;
-        pq.push(end);
-       
-        if(pq.top() <= start) {
-            pq.pop();
-        }
+   pq.push(v[0].second);
+
+   for (int i = 1; i < v.size(); i++)
+   {
+
+      int start = v[i].first;
+      int end = v[i].second;
+      pq.push(end);
+
+      if (pq.top() <= start)
+      {
+         pq.pop();
+      }
    }
-    
+
    cout << pq.size();
 }
