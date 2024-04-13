e4c6fb1cae985aa8ae09c2546c5e8ed4818cbdf6
kche1106 <kche011106@gmail.com>
2024-04-13 10:57:21 +0900

    [BFS] 1240 (re) /

--- DataStructure/1240.cpp
+++ DataStructure/1240.cpp
@@ -11,62 +11,61 @@
 using namespace std;
 
 int n, m;
-vector<pair<int, int>> map[1001];
-queue<int> q;
-int visited[1001];
+//int tree[1010][1010];
+vector<pair<int, int>> tree[1010];
+int x, y;
+queue<pair<int, int>> q;
+int visited[1010];
 int res;
 
-void BFS(int start, int dest) {
-    queue<pair<int, int>> q;
-    q.push({start, 0});
-    visited[start] = 1;
+void bfs()
+{
+
     res = 0;
-    
-    while(!q.empty()) {
-        int x = q.front().first;
+    while (!q.empty())
+    {
+        int node = q.front().first;
         int dist = q.front().second;
         q.pop();
-        
-        if(x == dest) {
+
+        // cout << "node = " << node << " dist = " << dist << endl;
+
+        if (node == y) {
             res = dist;
         }
-        
-        for(int i = 0; i < map[x].size(); i++) {            
-            int nx = map[x][i].first;
-            int ndist = map[x][i].second;
-            
-            if(!visited[nx]) {
+
+        for (int i = 0; i < tree[node].size(); i++) {
+            int nx = tree[node][i].first;
+            int ndist = tree[node][i].second;
+
+            if (!visited[nx])
+            {
                 visited[nx] = 1;
-                q.push({nx, ndist + dist});
+                q.push({nx, dist + ndist});
             }
         }
     }
+
 }
 
 int main() {
     cin >> n >> m;
-    
-    for(int i = 0; i < n-1; i++) {
-        int a, b, d;
-        cin >> a >> b >> d;
-        map[a].push_back({b, d});
-        map[b].push_back({a, d});
+
+    int a, b, c;
+    for (int i = 0; i < n-1; i++) {
+        cin >> a >> b >> c;
+        tree[a].push_back({b, c});
+        tree[b].push_back({a, c});
     }
-    
-    for(int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
+
+    while (m--) {
+        cin >> x >> y;
         
-        BFS(a, b);
-        cout << res << endl;
+        visited[x] = 1;
+        q.push({x, 0});
+        bfs();
+
+        cout << res << "\n";
         memset(visited, 0, sizeof(visited));
     }
 }
-/*
- 4 2
- 2 1 2
- 4 3 2
- 1 4 3
- 1 2
- 3 2
- */
